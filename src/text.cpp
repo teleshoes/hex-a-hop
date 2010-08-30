e9fd481eb4a25cb315c564beb6766cb865480df7
Ari Mustonen <amuzen@users.sourceforge.net>
2010-08-30 16:44:19 +0000

    Applied an iconv() portability patch received via email.

--- src/text.cpp
+++ src/text.cpp
@@ -443,7 +443,7 @@ void ConvertToUTF8(const std::string &text_locally_encoded, char *text_utf8, siz
 	errno = 0;
 	static const char *locale_enc = gettext_init.GetEncoding();
 	iconv_t cd = iconv_open("UTF-8", locale_enc);
-	char *in_buf = const_cast<char *>(&text_locally_encoded[0]);
+	ICONV_CONST char *in_buf = const_cast<char *>(&text_locally_encoded[0]);
 	char *out_buf = &text_utf8[0];
 	iconv(cd, &in_buf, &text_length, &out_buf, &text_utf8_length);
 	iconv_close(cd);
